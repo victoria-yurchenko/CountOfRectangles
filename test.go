2e08e02ae4b5f2d6c1d1ca89292d9f2041538042
xxwonderwallxx <vika010501@gmail.com>
2023-02-22 02:11:31 +0200

    Cleaned-up

--- test.go
+++ test.go
@@ -5,10 +5,10 @@ import (
 	"math/rand"
 )
 
-func sortByX(points [][]int) [][]int {
+func sortByX(points [][]int, length int) [][]int {
 
-	for i := 0; i < len(points); i++ {
-		for j := 0; j < len(points); j++ {
+	for i := 0; i < length; i++ {
+		for j := 0; j < length; j++ {
 			if points[0][i] > points[0][j] {
 				temp := points[0][i]
 				points[0][i] = points[0][j]
@@ -23,43 +23,23 @@ func sortByX(points [][]int) [][]int {
 func countRectangles(points [][]int, length int) int {
 
 	count := 0
+	points = sortByX(points, length)
 
-	for l := 0; l < length; l++ {
+	for k := 0; k < length; k++ {
 
-		currentX := points[0][l]
-		currentY := points[1][l]
+		// 0 - x, 1 - y coordinates
+		currentX := points[0][k]
+		currentY := points[1][k]
 
-		for i := l + 1; i < length; i++ {
+		for i := k + 1; i < length; i++ {
 			for j := i + 1; j < length; j++ {
 
 				if points[0][i] == currentX {
-
-					y2 := points[1][i]
-
-					for k := i + 1; k < length; k++ {
-						if points[1][i] == currentY {
-
-							if contains(points, length, points[0][i], y2) {
-								count++
-							}
-							break
-						}
-					}
+					count = getRectangleCount(points, i, length, currentY, count, 1, 0)
 				}
 
 				if points[1][i] == currentY {
-
-					x2 := points[0][i]
-
-					for k := i + 1; k < length; k++ {
-						if points[0][i] == currentX {
-
-							if contains1(points, length, points[1][i], x2) {
-								count++
-							}
-							break
-						}
-					}
+					count = getRectangleCount(points, i, length, currentX, count, 0, 1)
 				}
 			}
 		}
@@ -68,10 +48,25 @@ func countRectangles(points [][]int, length int) int {
 	return count
 }
 
-func contains(points [][]int, length, x, y int) bool {
+func getRectangleCount(points [][]int, i int, length int, checkingCoordinate int, count int, index1 int, index2 int) int {
+
+	currentCoordinate := points[index1][i]
+
+	for j := i + 1; j < length; j++ {
+		if points[index1][i] == checkingCoordinate {
+			if contains(points, length, points[index2][i], currentCoordinate, index2, index1) {
+				count++
+			}
+			break
+		}
+	}
+	return count
+}
+
+func contains(points [][]int, length int, coordinate1 int, coordinate2 int, index1 int, index2 int) bool {
 
 	for i := 0; i < length; i++ {
-		if points[0][i] == x && points[1][i] == y {
+		if points[index1][i] == coordinate1 && points[index2][i] == coordinate2 {
 			return true
 		}
 	}
@@ -96,22 +91,10 @@ func printPoints(points [][]int, length int) {
 	fmt.Println()
 }
 
-func contains1(points [][]int, length, x, y int) bool {
-
-	for i := 0; i < length; i++ {
-		if points[1][i] == x && points[0][i] == y {
-			return true
-		}
-	}
-
-	return false
-}
-
 func main() {
 
 	const countPoints = 2 // coordinates x, y
-	//length := rand.Intn(20)
-	length := 13
+	length := rand.Intn(50)
 	points := make([][]int, countPoints)
 
 	for y := 0; y < countPoints; y++ {
@@ -121,16 +104,6 @@ func main() {
 	for y := 0; y < countPoints; y++ {
 		for x := 0; x < length; x++ {
 			points[y][x] = rand.Intn(10)
-		}
-	}
-
-	for i := 0; i < length; i++ {
-		for j := 0; j < length; j++ {
-			if points[0][i] < points[0][j] {
-				temp := points[0][i]
-				points[0][i] = points[0][j]
-				points[0][j] = temp
-			}
 		}
 	}
 
